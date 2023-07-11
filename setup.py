ede786c633817d0bb8557688e532c746a701e3ea
carnivore7777 <antgiannoulopoulos@gmail.com>
2023-07-11 19:30:00 +0100

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='exerevnetes',
+    version='0.1.0',
+    author='Antonios Giannoulopoulos',
+    description='Another auto machine learning implementation',
+    packages=find_packages(),
+    install_requires=[
+        'dependency1',
+        'dependency2',
+    ],
+)
