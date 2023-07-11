73da87b641a02429e2a97a0e8fe0ff62f211e307
carnivore7777 <antgiannoulopoulos@gmail.com>
2023-07-11 21:00:28 +0100

    Updating setup.py Adding version file

--- setup.py
+++ setup.py
@@ -1,9 +1,30 @@
 from setuptools import setup, find_packages
 
+VERSIONFILE = "./exerevnetes/_version.py"
+versionstr = open(VERSIONFILE, "rt").read()
+
+if versionstr:
+    version = versionstr.split()[-1].strip('"')
+else:
+    raise RuntimeError(f"Unable to find version in {VERSIONFILE}.")
+
+with open("README.md", "rb") as fh:
+    long_description = fh.read().decode('utf-8', errors='ignore')
+
 setup(
     name='exerevnetes',
-    version='0.1.0',
+    version=version,
     author='Antonios Giannoulopoulos',
-    description='Another auto machine learning implementation',
-    packages=find_packages()
+    author_email="antgiannoulopoulos@gmail.com",
+    description='Another auto machine learning implementation, the goal is to create a simple yet comperhensive library that automates model selection',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/carnivore7777/exerevnetes",
+    packages=find_packages(),
+    install_requires=[
+        "sklearn",
+        "pandas",
+        "skopt"
+    ]
 )
+
