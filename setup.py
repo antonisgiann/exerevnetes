36b5660b244b92218f4e004c24c3a87b53970fa7
carnivore7777 <antgiannoulopoulos@gmail.com>
2023-07-11 20:59:58 +0100

    Adding testing.ipynb to gitignore Adding poetry files for local development

--- setup.py
+++ setup.py
@@ -5,9 +5,5 @@ setup(
     version='0.1.0',
     author='Antonios Giannoulopoulos',
     description='Another auto machine learning implementation',
-    packages=find_packages(),
-    install_requires=[
-        'dependency1',
-        'dependency2',
-    ],
+    packages=find_packages()
 )
