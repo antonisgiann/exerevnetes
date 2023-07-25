4b1ebde35071787a7abc28e3b4b95e6f76bf6d73
carnivore7777 <antgiannoulopoulos@gmail.com>
2023-07-25 01:01:58 +0100

    Adding general test dataset, adding test for multiple classification for binary comparator

--- exerevnetes/classification/tests/test_clf_comparator.py
+++ exerevnetes/classification/tests/test_clf_comparator.py
@@ -2,9 +2,13 @@ import pytest
 import numpy as np
 
 from .._clf_comparator import BinaryClassifierComparator
+from sklearn.datasets import make_classification
 
 
 np.random.seed(47)
+
+# General testing dataset
+X, y = make_classification(n_samples=1000, n_features=10, n_informative=5, n_classes=2, random_state=47)
 
 
 @pytest.mark.parametrize(
@@ -31,11 +35,15 @@ def test_default_metrics_size(X, y, cv, expected):
 
 @pytest.mark.parametrize(
     "X, y",
-    [(np.random.randint(0, 100, size=1000).reshape(100,10), np.random.randint(0, 1, size=100))]
+    [
+        (np.random.randint(0, 100, size=1000).reshape(100,10), np.random.randint(0, 1, size=100)),
+        (np.random.randint(0, 100, size=1000).reshape(100,10), np.random.randint(0, 5, size=100))
+    ]
 )
-def test_single_class(X, y):
-    """test if there is less than two classes in y_true."""
-    cmp = BinaryClassifierComparator(X, y)
+def test_n_class(X, y):
+    """test if there are two classes in y_true."""
     with pytest.raises(ValueError) as exce:
-        cmp.run()
-    assert str(exce.value) == "Expected 2 classes but got less in y_true"
+        cmp = BinaryClassifierComparator(X, y)
+    assert str(exce.value).startswith("Expected 2 classes but got")
+
+
