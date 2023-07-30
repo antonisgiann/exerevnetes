1bfce577d9eaae43cbb912e0d298ff80972c246a
carnivore7777 <antgiannoulopoulos@gmail.com>
2023-07-30 12:01:10 +0100

    Added a function to check constructor arguments of BinaryClassifierComparator

--- exerevnetes/classification/_clf_comparator.py
+++ exerevnetes/classification/_clf_comparator.py
@@ -40,17 +40,16 @@ default_metric_funcs = [
 
 class BinaryClassifierComparator:
     def __init__(self, X, y, classifiers: dict = default_classifiers, cv=5, metric_funcs: list = default_metric_funcs, preprocess=None):
+        self.__check_constructor_args(locals())
         self.X = X
         self.n_classes = len(Counter(y))
-        if self.n_classes != 2:
-            raise ValueError(f"Expected 2 classes but got {self.n_classes}.")
         self.y = y
         self.classifiers = classifiers
         self.cv = cv
         self.metric_funcs = metric_funcs
         self.preprocess = preprocess
         self._metrics = {}
-        if self.__preprocess_check(self.preprocess):
+        if self.preprocess:
             self.__build_pipelines(self.preprocess)
     
     def run(self):
@@ -71,11 +70,37 @@ class BinaryClassifierComparator:
         self.__format_metrics()
         display(self._metrics)
 
+    def __check_constructor_args(self, args):
+        if len(args["X"]) == 0:
+            raise ValueError("An empty array was given for 'X'")
+        if len(args["y"]) != len(args["X"]):
+            raise ValueError(f"There is a mismatch between 'X' and 'y'. 'X' has shape {args['X'].shape} and 'y' has shape {args['y'].shape}")
+        if len(args["classifiers"]) == 0:
+            arg_type = str(type(args['classifiers'])).split()[-1][1:-2]
+            if arg_type != "dict":
+                type_msg = "Be aware that 'classifiers' must be a dictionary"
+            else:
+                type_msg = ""
+            raise ValueError(f"An empty {arg_type} was given for 'classifiers'. {type_msg}")
+        if len(args["metric_funcs"]) == 0:
+            arg_type = str(type(args['metric_funcs'])).split()[-1][1:-2]
+            if arg_type != "list":
+                type_msg = "Be aware that 'metric_funcs' must be a list"
+            else:
+                type_msg = ""
+            raise ValueError(f"An empty {arg_type} was given for 'metric_funcs'. {type_msg}")
+        n_classes = Counter(args['y'])
+        if len(n_classes) != 2:
+            raise ValueError(f"Expected 2 classes but got {n_classes}.")
+        self.__preprocess_check(args['preprocess'])
+
     def __preprocess_check(self, preprocess):
+        if preprocess == None:
+            return True
         if type(preprocess) != Pipeline:
                 raise AttributeError(f"The comparator accepts only \033[34msklearn.pipeline.Pipeline\033[0m objects as 'preprocess'.")
         elif hasattr(preprocess.steps[-1][1], "predict"):
-                raise AttributeError("The 'preprocess' contains an predictor. Please make sure 'preprocess' contains only preprocessing steps")
+                raise AttributeError("The 'preprocess' contains a predictor. Please make sure 'preprocess' contains only preprocessing steps")
         else:
             return True
         
@@ -93,6 +118,8 @@ class BinaryClassifierComparator:
             self.classifiers[clf_name] = tmp_pipe
 
     def set_preprocess(self, preprocess):
+        if preprocess == None:
+            raise ValueError("'preprocess' is None")
         if self.__preprocess_check(preprocess):
             self.preprocess = preprocess
             self.__build_pipelines(self.preprocess)
