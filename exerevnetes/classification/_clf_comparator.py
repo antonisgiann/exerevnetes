c6e8ba4e811ce1b3ced95ecbf599625ac8f0b6d2
carnivore7777 <antgiannoulopoulos@gmail.com>
2023-08-02 20:26:09 +0100

    Adding BinaryClassifierComparator doc string

--- exerevnetes/classification/_clf_comparator.py
+++ exerevnetes/classification/_clf_comparator.py
@@ -41,12 +41,79 @@ default_metric_funcs = [
 ]
 
 class BinaryClassifierComparator:
+    """Compare different classifiers with cross validation 
+    on different metrics and aggregate the results in a dataframe
+
+    Parameters
+    ----------
+    X:  {array-like matrix} of shape (n_samples, n_features)
+        Training vectors, where `n_samples` is the number of samples and
+        `n_features` is the number of features.
+        
+    y:  array-like of shape (n_samples,)
+        Target values.
+
+    classifiers: dictionary of {str: estimator}, default=None (there is predifined
+        dictionary of classifiers in that case)
+        Contains all the estimators to be compared. ``set_classifiers`` can
+        be used to midify that dictionary.
+
+    cv: integer, default=5
+        The number of folds for the cross validation
+
+    metric_funcs: list of metric functions that take (y_true, preds), default = None
+        (There is a default list of metric functions to be used in that case)
+        This list contains the metric functions to be used on the cross validation
+        results and the y_true.
+
+    preprocess: sklearn.pipeline.Pipeline or sklearn.compose.ColumnTransformer object, default = None
+        It is expected that this object contains only preprocessing steps.
+        You can define a different preprocessing by using the ``set_preprocess`` function
+
+    exclude: list of str, default = None
+        This list contains names of classifiers to be excluded from the comparator.
+
+    Examples
+    --------
+    >>> from sklearn.ensemble import RandomForestClassfier, ExtraTreesClassifier
+    >>> from sklearn.metrics import ruc_auc_score
+    >>> from sklearn.pipeline import Pipeline
+    >>> from exerevnetes import BinaryClassifierComparator
+    >>> X, y = make_classification(n_samples=1000, n_features=10, n_informative=5, n_classes=2, random_state=47)
+
+    Run with the default parameters
+
+    >>> cmp = BinaryClassifierComparator(X, y)
+    >>> cmp.run()
+    >>> cmp.get_results().shape
+    (6,7) 
+    >>> cmp.get_best_clf()
+    
+    Define the classifiers of your interest
+
+    >>> cmp.set_classifiers({"forest": RandomForestClassifier(), "extra": ExtraTreesClassifier()})
+    >>> cmp.run()
+
+    Define your own metrics or/and exclude a default classifier from your comparison
+
+    >>> def my_function(y_true, preds):
+    ...     something
+    ...     something
+    ...     return score
+    >>> cmp = BinaryClassifierComparator(X, y, metric_funcs = [my_function, roc_auc_score], exclude = ["svc"])
+    >>> cmp.run()
+
+    Add a pipeline, or change the pipeline being used
+    >>> my_pipe = Pipeline(steps=[("step1", step1), ("step2", step2)])
+    >>> cmp.set_preprocess(my_pipe)
+    >>> cmp.run()
+    """
     def __init__(
             self, 
             X, 
             y, 
             classifiers: dict = None, 
-            cv: int =5, 
+            cv: int = 5, 
             metric_funcs: list = default_metric_funcs, 
             preprocess=None, 
             exclude: list = None
