12ce43189e7713e567bf7d87319ac2979b79727d
carnivore7777 <antgiannoulopoulos@gmail.com>
2023-08-09 12:00:20 +0300

    moving utils to a folder to accomodate refactoring

--- exerevnetes/utils.py
+++
@@ -1,6 +0,0 @@
-def time_format(t):
-    hours = int(t//3600)
-    minutes = int((t - hours * 3600) // 60)
-    seconds = int(t - (hours * 3600 + minutes * 60))
-    
-    return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
