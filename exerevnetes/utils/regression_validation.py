cd2e9db57a4a8b3b3843d218a79cc4536f36fc4e
carnivore7777 <antgiannoulopoulos@gmail.com>
2023-08-21 21:06:37 +0300

    Adding first draft of RegressionValidator

--- exerevnetes/utils/regression_validation.py
+++ exerevnetes/utils/regression_validation.py
@@ -1,4 +1,5 @@
 from exerevnetes.base_validation import Validator
+from collections import Counter
 
 from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
 from sklearn.svm import SVR
@@ -17,3 +18,23 @@ default_estimators = {
     "xgboost": XGBRegressor(),
     "catboost": CatBoostRegressor(verbose=False, allow_writing_files=False),
 }
+
+class RegressionValidator(Validator):
+    def __init__(self, X=None, y=None, estimators=None, cv=None, metric_funcs=None, preprocess=None, exclude=None):
+        super().__init__(X, y, estimators, cv, metric_funcs, preprocess, exclude)
+
+    @classmethod
+    def __validate_regression(cls, y):
+        if y is not None:
+            n_classes = Counter(y)
+            if len(n_classes) <= 100 or isinstance(y, float):
+                raise ValueError(f"`y` looks like a discrete variable.`")
+        return True
+    
+    def validate(self):
+        base_val = self.run_base_validation()
+        if self.y is not None:
+            base_val = base_val and self.__validate_regression(self.y) 
+        if self.exclude is not None:
+            base_val = base_val and self.__validate_exclude_param(self.estimators, self.exclude)
+        return base_val
