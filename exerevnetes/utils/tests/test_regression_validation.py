4ea063561e7fd3670cbe3ebd9ba276d8ab9a14e5
carnivore7777 <antgiannoulopoulos@gmail.com>
2023-08-22 17:42:02 +0300

    Cleaning up imports for now

--- exerevnetes/utils/tests/test_regression_validation.py
+++ exerevnetes/utils/tests/test_regression_validation.py
@@ -1,10 +1,7 @@
 import pytest
 from exerevnetes.utils.regression_validation import RegressionValidator
 from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
-from sklearn.metrics import f1_score, roc_auc_score
-from sklearn.pipeline import Pipeline
 from sklearn.datasets import make_regression
-from sklearn.preprocessing import StandardScaler
 
 
 X, y = make_regression(n_samples=200, n_features=10)
